3d5c77f9c96f0a381a118b738536affdfc0c3b1c
navyaravula2310 <95956370+navyaravula2310@users.noreply.github.com>
2022-01-05 15:05:06 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,22 @@
+class A:
+     def m1(self):
+         print("m1 of A class")
+    
+     def m2(self):
+         print("m2 of A class")
+class B(A):
+     def m3(self):
+         print("m3 of B class")
+    
+     def m4(self):
+         print("M4 of class B")
+
+
+obj=B();
+
+obj.m1()
+
+
+
+
+
